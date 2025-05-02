75b4c550ec58bfeaa0efb962a1a84b4c583d817c
Ale Almaraz <ale@almaraz.dev>
2025-05-02 12:10:18 -0600

    Update main.tf

--- azure/_modules/aks/main.tf
+++ azure/_modules/aks/main.tf
@@ -39,6 +39,7 @@ resource "azurerm_kubernetes_cluster" "TerraFailAKS_cluster" {
   }
 }
 
+
 resource "azurerm_kubernetes_cluster_node_pool" "TerraFailAKS_node_pool" {
   name                  = "TerraFailAKS_node_pool"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.TerraFailAKS_cluster.id
